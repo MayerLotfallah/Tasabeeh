9536acc7c2e1c0bd547eefc4cb5b9bc14ab6a4f3
MayerLotfallah <mayer.lotfallah48@gmail.com>
2026-06-25 00:50:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+const CACHE_NAME = 'TasabehCache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.html',
+  '/artist.html',
+  '/login.html',
+  'app.css',
+  'artist.css',
+  'playlist.html',
+  'see-all-artistpage.html',
+  'see-all-playlistpage.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
